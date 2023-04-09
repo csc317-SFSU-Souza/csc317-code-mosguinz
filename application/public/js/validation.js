46ec69606dd3c31c00de4b386d7cbf62d5c21a3d
mosguinz <mos.guinz@gmail.com>
2023-04-09 13:50:13 -0700

    Add method to validate password

--- application/public/js/validation.js
+++ application/public/js/validation.js
@@ -12,3 +12,22 @@ function validateUsername(username) {
         isAlphanumeric: isAlphanumeric
     };
 }
+
+/**
+ * @param    {string}    s
+ */
+function validatePassword(s) {
+    const meetsLength = s.length >= 8;
+    const containsUppercase = s.match(/[A-Z]/g);
+    const containsDigit = s.match(/[0-9]/g);
+    const containsSpecials = s.match(/[\/\*\-\+\!\@\#\$\^\&\~\[\]]/g);
+    return {
+        valid: meetsLength && containsUppercase && containsDigit && containsSpecials,
+        meetsLength: meetsLength,
+        containsUppercase: containsUppercase,
+        containsDigit: containsDigit,
+        containsSpecials: containsSpecials
+    };
+}
+
+
