284b9214b354c3a27c5a70a5e9cd20f7a38ac470
mosguinz <mos.guinz@gmail.com>
2023-05-22 14:08:03 -0700

    Add endpoint for handling user registration

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../conf/database");
+var bcrypt = require("bcrypt");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/register", async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    try {
+        var [rows, fields] = await db.execute(`SELECT id FROM users WHERE username=?;`, [username]);
+        if (rows && rows.length) {
+            return res.redirect("/registration");
+        }
+        var [rows, fields] = await db.execute(`SELECT id FROM users WHERE email=?;`, [email]);
+        if (rows && rows.length) {
+            return res.redirect("/registration");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 3);
+        var [resObject, fields] = db.execute(`INSERT INTO users
+        (username, email, password) value (?, ?, ?)`, [username, email, hashedPassword]);
+
+        // send client resp
+        if (resObject && resObject.affectedRows) {
+            return res.redirect("/login");
+        } else {
+            return res.redirect("/registration");
+        }
+    } catch (err) {
+        next(err);
+    }
+    res.end();
 });
 
 module.exports = router;
