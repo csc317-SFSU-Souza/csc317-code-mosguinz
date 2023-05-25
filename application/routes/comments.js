28de5af325d60ce08253c8620d39ea00f0aa2f76
mosguinz <mos.guinz@gmail.com>
2023-05-24 22:20:23 -0700

    Add route to post comment

---
+++ application/routes/comments.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+var { isLoggedIn } = require("../middleware/auth");
+
+const Comment = require("../models/comment");
+
+router.post("/create", isLoggedIn, async (req, res, next) => {
+    const { userId, username } = req.session.user;
+    const { postId, content } = req.body;
+
+    try {
+        const resObject = await Comment.postComment(comment, postId, userId);
+        if (resObject && resObject.affectedRows == 1) {
+            return res.status(201).json({
+                commentId: resObject.insertId,
+                username: username,
+                content: content
+            });
+        }
+        return res.json({
+            message: "Failed to post comment"
+        });
+    } catch (err) {
+        return next(err);
+    }
+
+
+});
+
