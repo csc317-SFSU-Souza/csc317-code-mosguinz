91c0ca6b71a8cc752ad610abfc7211e8d3702793
mosguinz <mos.guinz@gmail.com>
2023-05-24 23:51:25 -0700

    Fix return type from db comment insert

--- application/models/comment.js
+++ application/models/comment.js
@@ -3,13 +3,13 @@ var db = require("../conf/database");
 class Comment {
 
     /**
-     * @returns {import("mysql2").OkPacket}
+     * @returns {import("mysql2").ResultSetHeader}
      */
     static async postComment(content, postId, userId) {
-        var [rows, _] = await db.execute(`INSERT INTO comments
+        var [res, _] = await db.execute(`INSERT INTO comments
         (content, fk_post, fk_author) VALUES (?,?,?);`,
             [content, postId, userId]);
-        return rows[0];
+        return res;
     }
 
 }
