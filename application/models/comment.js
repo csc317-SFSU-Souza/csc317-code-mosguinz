3f95dfe224d76e19fd3f7fd32c5e539cb1d3307b
mosguinz <mos.guinz@gmail.com>
2023-05-24 23:37:45 -0700

    Fix SQL statement for comment insert

--- application/models/comment.js
+++ application/models/comment.js
@@ -2,9 +2,12 @@ var db = require("../conf/database");
 
 class Comment {
 
+    /**
+     * @returns {import("mysql2").OkPacket}
+     */
     static async postComment(content, postId, userId) {
         var [rows, _] = await db.execute(`INSERT INTO comments
-        (content, fk_post, fk_author) VALUE ?,?,?;`,
+        (content, fk_post, fk_author) VALUES (?,?,?);`,
             [content, postId, userId]);
         return rows[0];
     }
