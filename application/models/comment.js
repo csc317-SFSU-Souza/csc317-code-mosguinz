0a79e8d1e6051c7bfc5b77dc79619f3f69292d6b
mosguinz <mos.guinz@gmail.com>
2023-05-24 22:02:04 -0700

    Add method to post comments

---
+++ application/models/comment.js
@@ -0,0 +1,12 @@
+var db = require("../conf/database");
+
+class Comment {
+
+    static async postComment(content, postId, userId) {
+        var [rows, _] = await db.execute(`INSERT INTO comments
+        (content, fk_post, fk_author) VALUE ?,?,?;`,
+            [content, postId, userId]);
+        return rows[0];
+    }
+
+}
